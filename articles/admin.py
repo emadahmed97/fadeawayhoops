89dba6aa6aa37ae6e2a60544be28d32c8a9c277b
emadahmed97 <emad1997@gmail.com>
2016-11-27 01:34:35 -0500

    staged admin

--- articles/admin.py
+++ articles/admin.py
@@ -5,14 +5,4 @@ admin.site.register(Metadata)
 
 
 
-# Register your models here.
-class ContentAdmin(admin.ModelAdmin):
-    date_hierarchy = 'article_date'
-    list_display = ('article_title', 'article_date','article_author')
-    prepopulated_fields = {"slug": ("article_title",)}
-    #list_display_links = ('')
-    #list_editable = ('article_title','article_author','article_author')
-    class Meta:
-        model = Content
-
-admin.site.register(Content, ContentAdmin)
+admin.site.register(Content)
